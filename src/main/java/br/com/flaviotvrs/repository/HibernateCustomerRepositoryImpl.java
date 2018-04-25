4e6bc9dc0231653ab309614a92dc7bcb52206cbb
Flávio Tavares <ftavares@ciandt.com>
2018-04-25 11:12:06 -0300

    including properties configuration into the application

--- src/main/java/br/com/flaviotvrs/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/br/com/flaviotvrs/repository/HibernateCustomerRepositoryImpl.java
@@ -3,6 +3,7 @@ package br.com.flaviotvrs.repository;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import br.com.flaviotvrs.model.Customer;
@@ -10,11 +11,15 @@ import br.com.flaviotvrs.model.Customer;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+	@Value("${dbUsername}")
+	private String dbUsername;
+	
 	/* (non-Javadoc)
 	 * @see br.com.flaviotvrs.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll(){
+		System.out.println("[DEBUG] Querying data from DB using username: " + dbUsername);
 		return Arrays.asList(new Customer("Flávio", "Tavares"));
 	}
 }
